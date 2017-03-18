23534a5c6c2b7c0fa21bde0c50b643b93db11908
Thodoris Karagiannis <thodoris.karagiannis@camelotglobal.com>
2017-03-18 17:46:22 +0200

    Added unit test.

--- src/test/java/gr/aueb/test/model/CartTest.java
+++ src/test/java/gr/aueb/test/model/CartTest.java
@@ -1,7 +1,21 @@
 package gr.aueb.test.model;
 
+import gr.aueb.mscis.sample.model.Cart;
+import gr.aueb.mscis.sample.util.SimpleCalendar;
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by thodoriskaragiannis on 16/03/2017.
  */
 public class CartTest {
+
+    @Test
+    public void getCartDate(){
+        Cart cr = new Cart();
+        Assert.assertNotNull(cr);
+        SimpleCalendar march_20_2017 = new SimpleCalendar(2017, 3 , 20);
+        cr.setCartDate(march_20_2017);
+        Assert.assertEquals(march_20_2017, cr.getCartDate());
+    }
 }
